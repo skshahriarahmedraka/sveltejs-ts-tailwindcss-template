d95c1d2519af75bb4cd326f1189328d3f27c2ba4
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 <skshahriarahmedraka@gmail.com>
2021-12-14 20:07:28 +0600

    14Dec21

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	// import ModeSwitcher from './ModeSwitcher.svelte';
+	import Tailwindcss from './Tailwindcss.svelte';
+	// export let name: string;
+</script>
+<style>
+	
+</style>
+<Tailwindcss />
+<!-- <ModeSwitcher /> -->
+<!-- <main class="p-4 mx-auto text-center max-w-xl">
+	<h1 class="uppercase text-6xl leading-normal font-thin text-svelte">Hello {name}!</h1>
+	<p class="custom-style mt-[3rem]">
+		Visit the
+		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main> -->
+
+
+<div class="bg-gray-200" >
+	<h1 class=" text-4xl">sk shahriar ahmed raka </h1>
+	<h1 class=" text-4xl">Svelte3 + Typescript + Tailwindcss3.0 </h1>
+</div>
